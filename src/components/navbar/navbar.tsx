1d057e54b96b09ffd6bda4f939fc418f2ad8a2fb
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2025-04-05 18:22:49 -0500

    asd

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,25 +1,36 @@
 'use client'
 
-const Navbar = () => {
+import { Facebook, Instagram, Twitter, Share2 } from 'lucide-react';
+import { CONTACT_INFO, ROUTES } from '@/utils/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
+const Navbar = () => {
+    const pathname = usePathname();
+
+    const isActive = (path: string) => {
+        if (path === ROUTES.HOME) {
+            return pathname === path ? 'text-blue-500 font-bold' : '';
+        }
+        return pathname.startsWith(path) ? 'text-blue-500 font-bold' : '';
+    };
 
     return (
         <nav >
             <div className="topbar-one">
                 <div className="container">
                     <div className="topbar-one__left">
-                        <a href="#">Inició de sesión</a>
-                        <a href="#">Ubicación</a>
+                        <Link href={ROUTES.LOGIN}>Inició de sesión</Link>
+                        <Link href={ROUTES.LOCATION}>Ubicación</Link>
                     </div>
                     <div className="topbar-one__social">
-                        <a href="#"><i className="fab fa-facebook-square"></i></a>
-                        <a href="#"><i className="fab fa-twitter"></i></a>
-                        <a href="#"><i className="fab fa-pinterest-p"></i></a>
-                        <a href="#"><i className="fab fa-instagram"></i></a>
+                        <a href={CONTACT_INFO.SOCIAL_MEDIA.FACEBOOK} target="_blank" rel="noopener noreferrer"><Facebook className="w-5 h-5" /></a>
+                        <a href={CONTACT_INFO.SOCIAL_MEDIA.TRIPADVISOR} target="_blank" rel="noopener noreferrer"><Twitter className="w-5 h-5" /></a>
+                        <a href={CONTACT_INFO.SOCIAL_MEDIA.FLICKR} target="_blank" rel="noopener noreferrer"><Share2 className="w-5 h-5" /></a>
+                        <a href={CONTACT_INFO.SOCIAL_MEDIA.INSTAGRAM} target="_blank" rel="noopener noreferrer"><Instagram className="w-5 h-5" /></a>
                     </div>
                 </div>
             </div>
-
             <nav className="main-nav-one stricky">
                 <div className="container">
                     <div className="main-nav-one__infos">
@@ -28,35 +39,55 @@ const Navbar = () => {
                     </div>
                     <div className="inner-container">
                         <div className="logo-box flex justify-center items-center">
-                        <a href="index.html" className="flex justify-center">
-                           {/*  <img src="assets/images/logo-1-1.png" alt="" width="143"/> */}
-                        </a>
+                            <a href="index.html" className="flex justify-center">
+                                {/*  <img src="assets/images/logo-1-1.png" alt="" width="143"/> */}
+                            </a>
                             <a href="#" className="side-menu__toggler"><i className="fa fa-bars"></i></a>
                         </div>
                         <div className="main-nav__main-navigation">
                             <ul className="main-nav__navigation-box">
+                                <li className="dropdown">
+                                    <Link 
+                                        href={ROUTES.HOME} 
+                                        className={`transition-colors duration-200 hover:text-blue-500 ${isActive(ROUTES.HOME)}`}
+                                    >
+                                        Inicio
+                                    </Link>
+                                </li>
+                                <li className="dropdown">
+                                    <Link 
+                                        href={ROUTES.ABOUT} 
+                                        className={`transition-colors duration-200 hover:text-blue-500 ${isActive(ROUTES.ABOUT)}`}
+                                    >
+                                        Nosotros
+                                    </Link>
+                                </li>
+                                <li>
+
+                                </li>
+                                <li className="dropdown">
+                                    <Link 
+                                        href={ROUTES.COURSES} 
+                                        className={`transition-colors duration-200 hover:text-blue-500 ${isActive(ROUTES.COURSES)}`}
+                                    >
+                                        Cursos
+                                    </Link>
+                                </li>
 
                                 <li className="dropdown">
-                                    <a href="index.html">Home</a>
+                                    <Link 
+                                        href={ROUTES.CONTACT} 
+                                        className={`transition-colors duration-200 hover:text-blue-500 ${isActive(ROUTES.CONTACT)}`}
+                                    >
+                                        Contactos
+                                    </Link>
                                 </li>
 
-                                <li className="dropdown">
-                                    <a href="about.html">About</a>
-                                </li>
-                                <li></li>
-                                <li className="dropdown">
-                                    <a href="courses.html">Courses</a>
-                                </li >
-                                <li className="dropdown">
-                                    <a href="contact.html">Contact</a>
-                                </li>
-                          
                             </ul>
                         </div>
                     </div>
                 </div>
             </nav>
-
         </nav>
     );
 };
