eb0c99b13054d216408043b08bbcc128f19ec871
Javier Garcia Carrillo <50500251+javiergcw@users.noreply.github.com>
2025-12-10 16:04:52 -0500

    with version

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '154.38.181.22',
-        port: '9000',
-        pathname: '/oceanoscuba/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'gateway.makerstech.co',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 's3.makerstech.co',
-        pathname: '/**',
-      },
-    ],
+    domains: ['gateway.makerstech.co', 's3.makerstech.co'],
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
